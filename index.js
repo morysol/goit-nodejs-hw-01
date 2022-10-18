fe47af2f9864a1dcbba02248bbb04d76e1736a5b
Sergey Smirnov <cepera.cMupHoB@gmail.com>
2022-10-18 22:53:34 +0300

    fix

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
-// import { testImport } from "./contacts.js";
 const { Command } = require("commander");
 const contacts = require("./contacts");
 
 const program = new Command();
-
-// (async () => {
-//   // …
-//   //   console.log(await contacts.getContacts());
-//   const allContacts = await contacts.getContacts();
-//   //   console.log(allContacts);
-//   contacts.listContacts(allContacts);
-//   console.log(contacts.getContactById(5, allContacts));
-//   console.table(contacts.removeContact(3, allContacts));
-
-//   const email = "mango@gmail.com";
-//   const phone = "322-22-22";
-//   const name = "Mango";
-
-//   console.table(contacts.addContact(name, email, phone, allContacts));
-// })();
-
-// # Получаем и выводим весь список контактов в виде таблицы (console.table)
-// node index.js --action list
-
-// # Получаем контакт по id
-// node index.js --action get --id 5
-
-// # Добавялем контакт
-// node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
-
-// # Удаляем контакт
-// node index.js --action remove --id=3
 
 program
   .option("-a, --action <type>", "choose action")
@@ -51,16 +22,11 @@ async function invokeAction({ action, id, name, email, phone }) {
       break;
 
     case "get":
-      // ... id
       console.log(contacts.getContactById(id, await contacts.getContacts()));
 
       break;
 
     case "add":
-      // ... name email phone
-      //   console.table(
-      //     contacts.addContact(name, email, phone, await contacts.getContacts())
-      //   );
       const listAdd = contacts.addContact(
         name,
         email,
@@ -72,13 +38,11 @@ async function invokeAction({ action, id, name, email, phone }) {
       break;
 
     case "remove":
-      // ... id
-      //   console.table(contacts.removeContact(id, await contacts.getContacts()));
       const listRemove = contacts.removeContact(
         id,
         await contacts.getContacts()
       );
-      //   console.table(JSON.stringify(list));
+
       await contacts.saveContacts(JSON.stringify(listRemove));
       break;
 
