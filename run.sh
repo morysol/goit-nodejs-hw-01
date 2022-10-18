d32f2312ee4f1bdec2ad5ef91a770a7ff9507f6b
Sergey Smirnov <cepera.cMupHoB@gmail.com>
2022-10-18 23:55:18 +0300

    fix

---
+++ run.sh
@@ -0,0 +1,23 @@
+### Run
+
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+node index.js --action list
+
+# Получаем контакт по id
+
+node index.js --action get --id 5 && node index.js --action list
+node index.js --action get --id 2 && node index.js --action list
+node index.js --action get --id 8 && node index.js --action list
+
+# Добавялем контакт
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22 && node index.js --action list
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22 && node index.js --action list
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22 && node index.js --action list
+
+# Удаляем контакт
+
+node index.js --action remove --id=3 && node index.js --action list
+node index.js --action remove --id=4 && node index.js --action list
+node index.js --action remove --id=7 && node index.js --action list
